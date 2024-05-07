b68735ca6a4509a90ee875e731d0506508d2467a
Royal Finch <Royal2791@gmail.com>
2024-05-06 20:17:18 -0400

    add css styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ export default function App() {
   return (
     <>
       <header className="header">
-        <h1>
+        <h1 className="header__heading">
           Reliable, efficient delivery{' '}
-          <span className="header-accent">Powered by Technology</span>
+          <span className="bold-accent">Powered by Technology</span>
         </h1>
-        <p>
+        <p className="header__paragraph">
           Our Artificial Intelligence powered tools use millions of project data
           points to ensure that your project is successful
         </p>
@@ -19,6 +19,8 @@ export default function App() {
             title="Supervisor"
             imgSrc="/images/icon-supervisor.svg"
             alt="supervisor icon"
+            color="hsl(180, 62%, 55%)"
+            gridArea="grid-left"
           >
             Monitors activity to identify project roadblocks
           </Card>
@@ -26,16 +28,26 @@ export default function App() {
             title="Team Builder"
             imgSrc="/images/icon-team-builder.svg"
             alt="team builder icon"
+            color="hsl(0, 78%, 62%)"
+            gridArea="grid-top"
           >
             Scans our talent network to create the optimal team for your project
           </Card>
-          <Card title="Karma" imgSrc="/images/icon-karma.svg" alt="karma icon">
+          <Card
+            title="Karma"
+            imgSrc="/images/icon-karma.svg"
+            alt="karma icon"
+            color="hsl(34, 97%, 64%)"
+            gridArea="grid-bottom"
+          >
             Regularly evaluates our talent to ensure quality
           </Card>
           <Card
             title="Calculator"
             imgSrc="/images/icon-calculator.svg"
             alt="calculator icon"
+            color="hsl(212, 86%, 64%)"
+            gridArea="grid-right"
           >
             Uses data from past projects to provide better delivery estimates
           </Card>
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,6 +1,16 @@
-export default function Card({ title, imgSrc, children, alt }) {
+export default function Card({
+  title,
+  imgSrc,
+  children,
+  alt,
+  color,
+  gridArea,
+}) {
   return (
-    <div className="card">
+    <div
+      className={`card ${gridArea}`}
+      style={{ borderTop: `4px solid ${color}` }}
+    >
       <h2 className="card__heading">{title}</h2>
       <p className="card__paragraph">{children}</p>
       <img className="card__img" src={imgSrc} alt={alt} />
